64fff16360a15ab6eb709e2415b18731e0edf590
dagpan <panagiotisntagkinis@gmail.com>
2020-02-19 12:17:36 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
+
 class App < Sinatra::Base
   get '/' do
     erb :index
@@ -8,7 +9,15 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    my_text = TextAnalyzer.new(text_from_user)
+    @word_count = my_text.count_of_words
+    @vowels_count = my_text.count_of_vowels
+    @consonants_count = my_text.count_of_consonants
+    hash1 = my_text.most_used_letter
+    hash1.each do |key, value| 
+          @letter = key.upcase
+          @times = value
+    end
     erb :results
   end
 end
